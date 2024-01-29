eee4214ca019f1ff27eeee61047cd717a5ce96a0
DesireeCSilva <desiresilva1987@gmail.com>
2024-01-29 21:08:55 +0100

    CRUD done

--- src/services.js
+++ src/services.js
@@ -12,7 +12,8 @@ async function printSeries(){
     `<h3>${serie.name}</h3>
     <p>${serie.genre}</p>
     <p>${serie.chapters}</p>
-    <button onclick="deleteSerie('${serie.id}')">Borrar</button>`
+    <button onclick="deleteSerie('${serie.id}')">Borrar</button>
+    <button onclick="modifySerie('${serie.id}')">Modificar</button>`
     })
 }
 
@@ -23,4 +24,43 @@ async function deleteSerie(id){
     {method: "DELETE"})
     return result
 }
+
+//Método POST C (CREATE) del CRUD con formulario
+
+async function createSerie() {
+    const formSerie = document.getElementById("series-form")
+
+    const newSerie = {
+        "name": formSerie.elements[0].value,
+        "genre": formSerie.elements[1].value,
+        "chapters": formSerie.elements[2].value
+    };
+
+    const result = await fetch(`http://localhost:3000/series` , {
+        method: "POST",
+        headers:{'Content-Type': 'application/json'},
+        body: JSON.stringify(newSerie),
+    });
+}
+
+// Método PUT U (update) del CRUD para modificar una serie por su id
+
+async function modifySerie(id) {
+    const newSerie = prompt('Modifica el nombre de la serie');
+    const newGenre = prompt('Modifica el género de la serie');
+    const newChapters = prompt('Modifica el número de capítulos');
+  
+    const response = await fetch(`http://localhost:3000/series/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: newSerie,
+            genre: newGenre,
+            chapters: newChapters,
+        }),
+    });
+}
+
 
